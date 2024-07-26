f9a25a4914e116a3486f7efd5192c5d4b4b41b01
Mahdi <mahdi.jebahi@gmail.com>
2024-07-26 14:37:07 +0200

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
